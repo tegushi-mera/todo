2b758c987059839ef1d1878c075ff239f290a9eb
admin <eirin.eralieva@gmail.com>
2021-01-26 16:26:40 +0600

    added models

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class ToDo(models.Model):
-    text = models.CharField(max_legnth=100)
+    text = models.CharField(max_length=100)
     created_at = models.DateField(auto_now_add=True)
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
